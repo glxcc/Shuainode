11d6a2f5f3e1ace83bbb0d1a8c2a92695ab28adf
glxcc <glxcc@yahoo.com>
2014-06-28 15:41:34 +0100

    bootstrap

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,24 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Shuai ge page' });
 });
+
+router.use('/login', function(req, res, next){
+    res.render('login', {title: 'login'})
+})
+
+router.get('/u/:user', function(req, res, next){
+    res.render('user', {title: req.params.user})
+})
+
+router.get('/reg', function(req, res, next){
+    res.render('reg', {title: 'register'})
+})
+
+router.get('/logout', function(req, res, next){
+    res.render('login',{title: 'you have logged out'})
+})
+
 
 module.exports = router;
