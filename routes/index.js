5ed9c38deecabd3bfa80a063c6f0f8249e18aac9
nlg <guolinxin@gmail.com>
2014-07-09 18:06:19 +0100

    router

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var router = express.Router();
+var app = express();
+// apply the routes to our application
+app.use('/', router);
 
 /* GET home page. */
 router.get('/', function(req, res) {
@@ -18,9 +21,11 @@ router.get('/', function(req, res) {
     );
 });
 
-router.use('/login', function(req, res, next){
+
+/*router.use('/login', function(req, res, next){
     res.render('login', {title: 'login'})
-})
+})*/
+
 
 router.get('/u/:user', function(req, res, next){
     res.render('user', {title: req.params.user})
@@ -34,5 +39,19 @@ router.get('/logout', function(req, res, next){
     res.render('login',{title: 'you have logged out'})
 })
 
+// apply the routes to our application
+app.use('/', router);
+
+app.route('/login')
+    // show the form (GET http://localhost:8080/login)
+    .get(function(req, res) {
+        res.render('login', {title: 'login'})
+    })
+
+    // process the form (POST http://localhost:8080/login)
+    .post(function(req, res) {
+        console.log('processing');
+        res.send('processing the login form!');
+    });
 
 module.exports = router;
