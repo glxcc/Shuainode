5c4eb7fb67c6a5e0da828ed6baff41771375bc17
nlg <guolinxin@gmail.com>
2014-07-09 17:33:48 +0100

    posts

--- models/post.js
+++ models/post.js
@@ -1,6 +1,16 @@
 /**
  * Created by nlg on 08/07/2014.
  */
-var db = require('./database');
 
-function User(){}
+var db = require('./database');
+var Schema = mongoose.Schema;
+
+var postSchema = new Schema({
+    name:{ type:String, default:''},
+    title:{type:String, default:''},
+    message:{type:String, default:''},
+    comments:[{body: String, date:Date}],
+    user:Schema.objectId
+});
+
+mongoose.model('post', postSchema);
